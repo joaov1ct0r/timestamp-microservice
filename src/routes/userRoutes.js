2052ed8903df15fbe053999a773f68cd25f0ad70
Joao Victor <jov9051@outlook.com>
2022-04-02 16:57:36 -0300

    feat: funcao handleNullDate sendo utilizada na rota /timestamp

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -4,4 +4,6 @@ import { handleDate, handleNullDate } from '../controller/userController.js';
 
 const userRouter = express.Router();
 
+userRouter.get('/timestamp', handleNullDate);
+
 export default userRouter;
